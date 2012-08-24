30388f4c03bae91a62172def23b5a4f463b2b2ff
Jake Pearson <pearsonjj@yahoo.com>
2012-08-23 21:20:42 -0600

    Added file diff utility

---
+++ directory-diff/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace directory_diff
+{
+	public static class Extension
+	{
+		public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+		{
+			foreach (T t in source)
+			{
+				action(t);
+			}
+		}
+	}
+}
