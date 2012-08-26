7b2d58db04330ce9792bb41e2f8a8a3b7a72ebd9
Jake Pearson <pearsonjj@yahoo.com>
2012-08-25 20:11:37 -0600

    More tests

---
+++ cde.district.validation/tests/AcademicGrowthAndGrowthGapsRating.cs
@@ -0,0 +1,16 @@
+﻿using cde.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cde.district.validation.tests
+{
+	public class AcademicGrowthAndGrowthGapsRating : BaseTest
+	{
+		public override void Test(Row row, List<string> errors)
+		{
+
+		}
+	}
+}
---
+++ cde.district.validation/tests/GrowthPointsEarnedEqualsPercentOfGrowthPoints.cs
@@ -0,0 +1,17 @@
+﻿using cde.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cde.district.validation.tests
+{
+	public class GrowthPointsEarnedEqualsPercentOfGrowthPoints : BaseTest
+	{
+		public override void Test(Row row, List<string> errors)
+		{
+			AssertDivide(row, "RDPF_1YR_GRO_PCT_PTS_EARN_TTL", "RDPF_1YR_GRO_PTS_EARN_TTL", "RDPF_1YR_GRO_PTS_ELIG_TTL", errors);
+			AssertDivide(row, "RDPF_3YR_GRO_PCT_PTS_EARN_TTL", "RDPF_3YR_GRO_PTS_EARN_TTL", "RDPF_3YR_GRO_PTS_ELIG_TTL", errors);
+		}
+	}
+}
