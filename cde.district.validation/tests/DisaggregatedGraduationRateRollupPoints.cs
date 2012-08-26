cc9f1012dcd53d67e773f57fe3df6eb8aa783695
Jake Pearson <pearsonjj@yahoo.com>
2012-08-25 20:48:41 -0600

    Added 2 more tests/fixed 1

---
+++ cde.district.validation/tests/DisaggregatedGraduationRateRollupPoints.cs
@@ -0,0 +1,18 @@
+﻿using cde.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cde.district.validation.tests
+{
+	public class DisaggregatedGraduationRateRollupPoints : BaseTest
+	{
+		public override void Test(Row row, List<string> errors)
+		{
+			AssertSum(row, "RDPF_1YR_PS_GRAD_DSAG_PTS_EARN", new[] { "RDPF_1YR_PS_GRAD_ELL_PTS_EARN", "RDPF_1YR_PS_GRAD_IEP_PTS_EARN", "RDPF_1YR_PS_GRAD_MIN_PTS_EARN", "RDPF_1YR_PS_GRAD_FRL_PTS_EARN" }, errors);
+			AssertSum(row, "RDPF_1YR_PS_GRAD_DSAG_PTS_ELIG", new[] { "RDPF_1YR_PS_GRAD_ELL_PTS_ELIG", "RDPF_1YR_PS_GRAD_IEP_PTS_ELIG", "RDPF_1YR_PS_GRAD_MIN_PTS_ELIG", "RDPF_1YR_PS_GRAD_FRL_PTS_ELIG" }, errors);
+			AssertSum(row, "RDPF_3YR_PS_GRAD_DSAG_PTS_EARN", new[] { "RDPF_3YR_PS_GRAD_ELL_PTS_EARN", "RDPF_3YR_PS_GRAD_IEP_PTS_EARN", "RDPF_3YR_PS_GRAD_MIN_PTS_EARN", "RDPF_3YR_PS_GRAD_FRL_PTS_EARN" }, errors);
+			AssertSum(row, "RDPF_3YR_PS_GRAD_DSAG_PTS_ELIG", new[] { "RDPF_3YR_PS_GRAD_ELL_PTS_ELIG", "RDPF_3YR_PS_GRAD_IEP_PTS_ELIG", "RDPF_3YR_PS_GRAD_MIN_PTS_ELIG", "RDPF_3YR_PS_GRAD_FRL_PTS_ELIG" }, errors);	}
+	}
+}
---
+++ cde.district.validation/tests/PostSecondaryGraduationRatingsAndDisaggregatedGraduationRateRatings.cs
@@ -0,0 +1,41 @@
+﻿using cde.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cde.district.validation.tests
+{
+	public class PostSecondaryGraduationRatingsAndDisaggregatedGraduationRateRatings : BaseTest
+	{
+		IEnumerable<Tuple<string, string>> Columns
+		{
+			get
+			{
+				yield return new Tuple<string, string>("RDPF_1YR_PS_GRAD_RATING", "RDPF_1YR_PS_GRAD_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_3YR_PS_GRAD_RATING", "RDPF_3YR_PS_GRAD_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_1YR_PS_GRAD_FRL_RATING", "RDPF_1YR_PS_GRAD_FRL_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_1YR_PS_GRAD_MIN_RATING", "RDPF_1YR_PS_GRAD_MIN_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_1YR_PS_GRAD_IEP_RATING", "RDPF_1YR_PS_GRAD_IEP_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_1YR_PS_GRAD_ELL_RATING", "RDPF_1YR_PS_GRAD_ELL_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_3YR_PS_GRAD_FRL_RATING", "RDPF_3YR_PS_GRAD_FRL_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_3YR_PS_GRAD_MIN_RATING", "RDPF_3YR_PS_GRAD_MIN_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_3YR_PS_GRAD_IEP_RATING", "RDPF_3YR_PS_GRAD_IEP_BEST_GR");
+				yield return new Tuple<string, string>("RDPF_3YR_PS_GRAD_ELL_RATING", "RDPF_3YR_PS_GRAD_ELL_BEST_GR");
+			}
+		}
+
+		public override void Test(Row row, List<string> errors)
+		{
+			Columns.ForEach(t => AssertRating(row, t.Item1, t.Item2, Rating, errors));
+		}
+
+		string Rating(double value)
+		{
+			if (value >= 90.0) return Exceeds;
+			if (value >= 80.0) return Meets;
+			if (value >= 65.0) return Approaching;
+			return DoesNotMeet;
+		}
+	}
+}
