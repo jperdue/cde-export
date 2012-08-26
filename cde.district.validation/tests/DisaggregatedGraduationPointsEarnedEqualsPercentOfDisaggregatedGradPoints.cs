1c0031e8b1303a37c79ef8d94e9a2fb116930e07
Jake Pearson <pearsonjj@yahoo.com>
2012-08-25 20:55:24 -0600

    Added 2 more

---
+++ cde.district.validation/tests/DisaggregatedGraduationPointsEarnedEqualsPercentOfDisaggregatedGradPoints.cs
@@ -0,0 +1,17 @@
+﻿using cde.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cde.district.validation.tests
+{
+	public class DisaggregatedGraduationPointsEarnedEqualsPercentOfDisaggregatedGradPoints : BaseTest
+	{
+		public override void Test(Row row, List<string> errors)
+		{
+			AssertDivide(row, "RDPF_1YR_PS_GRAD_DSAG_PCT_PTS", "RDPF_1YR_PS_GRAD_DSAG_PTS_EARN", "RDPF_1YR_PS_GRAD_DSAG_PTS_ELIG", errors);
+			AssertDivide(row, "RDPF_3YR_PS_GRAD_DSAG_PCT_PTS", "RDPF_3YR_PS_GRAD_DSAG_PTS_EARN", "RDPF_3YR_PS_GRAD_DSAG_PTS_ELIG", errors);
+		}
+	}
+}
---
+++ cde.district.validation/tests/DropoutRateRatings.cs
@@ -0,0 +1,33 @@
+﻿using cde.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cde.district.validation.tests
+{
+	public class DropoutRateRatings : BaseTest
+	{
+		public override void Test(Row row, List<string> errors)
+		{
+			AssertRating(row, "RDPF_1YR_PS_DROP_RATING", "RDPF_1YR_PS_DROP_RATE", Rating1, errors);
+			AssertRating(row, "RDPF_3YR_PS_DROP_RATING", "RDPF_3YR_PS_DROP_RATE", Rating3, errors);
+		}
+
+		string Rating1(double value)
+		{
+			if (value <= 1.0) return Exceeds;
+			if (value <= 3.6) return Meets;
+			if (value <= 10.0) return Approaching;
+			return DoesNotMeet;
+		}
+
+		string Rating3(double value)
+		{
+			if (value <= 1.0) return Exceeds;
+			if (value <= 3.9) return Meets;
+			if (value <= 10) return Approaching;
+			return DoesNotMeet;
+		}
+	}
+}
