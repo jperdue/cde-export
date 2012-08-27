7cff5873d2c47818cb496a96a6b05fdd3e0159d2
lysdawn <lysdawn@gmail.com>
2012-08-26 19:16:59 -0600

    replaced DN with NM

--- cde.district.validation/tests/CoAltOnePercentExceedsAdjustment.cs
+++ cde.district.validation/tests/CoAltOnePercentExceedsAdjustment.cs
@@ -11,14 +11,14 @@ namespace cde.district.validation.tests
 		{
 			get
 			{
-				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_READ", "RDPF_1YR_PARTIC_DN_READ", "RDPF_1YR_COALT_CAP_NEXCD_READ");
-				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_MATH", "RDPF_1YR_PARTIC_DN_MATH", "RDPF_1YR_COALT_CAP_NEXCD_MATH");
-				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_WRITE", "RDPF_1YR_PARTIC_DN_WRITE", "RDPF_1YR_COALT_CAP_NEXCD_WRITE");
-				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_SCI", "RDPF_1YR_PARTIC_DN_SCI", "RDPF_1YR_COALT_CAP_NEXCD_SCI");
-				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_READ", "RDPF_3YR_PARTIC_DN_READ", "RDPF_3YR_COALT_CAP_NEXCD_READ");
-				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_MATH", "RDPF_3YR_PARTIC_DN_MATH", "RDPF_3YR_COALT_CAP_NEXCD_MATH");
-				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_WRITE", "RDPF_3YR_PARTIC_DN_WRITE", "RDPF_3YR_COALT_CAP_NEXCD_WRITE");
-				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_SCI", "RDPF_3YR_PARTIC_DN_SCI", "RDPF_3YR_COALT_CAP_NEXCD_SCI");
+				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_READ", "RDPF_1YR_PARTIC_NM_READ", "RDPF_1YR_COALT_CAP_NEXCD_READ");
+				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_MATH", "RDPF_1YR_PARTIC_NM_MATH", "RDPF_1YR_COALT_CAP_NEXCD_MATH");
+				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_WRITE", "RDPF_1YR_PARTIC_NM_WRITE", "RDPF_1YR_COALT_CAP_NEXCD_WRITE");
+				yield return new Tuple<string, string, string>("RDPF_1YR_PARTIC_NM_NOCAP_SCI", "RDPF_1YR_PARTIC_NM_SCI", "RDPF_1YR_COALT_CAP_NEXCD_SCI");
+				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_READ", "RDPF_3YR_PARTIC_NM_READ", "RDPF_3YR_COALT_CAP_NEXCD_READ");
+				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_MATH", "RDPF_3YR_PARTIC_NM_MATH", "RDPF_3YR_COALT_CAP_NEXCD_MATH");
+				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_WRITE", "RDPF_3YR_PARTIC_NM_WRITE", "RDPF_3YR_COALT_CAP_NEXCD_WRITE");
+				yield return new Tuple<string, string, string>("RDPF_3YR_PARTIC_NM_NOCAP_SCI", "RDPF_3YR_PARTIC_NM_SCI", "RDPF_3YR_COALT_CAP_NEXCD_SCI");
 			}
 		}
 
