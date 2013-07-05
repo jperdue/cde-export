1dd556c5f22b7a6030e3a72cd7dc70207278ec95
Jake Pearson <pearsonjj@yahoo.com>
2013-07-05 15:11:34 -0600

    Participation Rate Tests (Issue #9)

---
+++ cde.district.validation/tests/ParticipationOverallMet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cde.district.validation.tests
+{
+    class ParticipationOverallMet : BaseTest
+    {
+        public IEnumerable<Tuple<string, string>> Columns
+        {
+            get
+            {
+                yield return new Tuple<string, string>("RDPF_1YR_TEST_PARTIC_DNM_COUNT", "RDPF_1YR_TEST_PARTIC_RATING");
+                yield return new Tuple<string, string>("RDPF_3YR_TEST_PARTIC_DNM_COUNT", "RDPF_3YR_TEST_PARTIC_RATING");
+                yield return new Tuple<string, string>("RDPF_1_3_PARTIC_DNM_COUNT", "RDPF_1_3_PARTIC_RATING");
+            }
+        }
+
+        public override void Test(Row row, Errors errors)
+        {
+            Columns.ForEach(t => AssertMet(row, t.Item1, t.Item2, errors));
+        }
+
+        bool AssertMet(Row row, string countColumn, string ratingColumn, Errors errors)
+        {
+            if(!Defined(row, countColumn, errors) &&
+                !Defined(row, ratingColumn, errors))
+            {
+                return true;
+            }
+
+            double count;
+            if (!AssertNumber(row, countColumn, out count, errors))
+            {
+                return false;
+            }
+
+            var rating = row[ratingColumn];
+            var expectedRating = count >= 2 ?  "Does Not Meet 95% Participation Rate" : "Meets 95% Participation Rate";
+            
+            return AssertTrue(row, rating == expectedRating, "Count does not line up with rating (" + ratingColumn + ")", errors);
+        }
+    }
+}
