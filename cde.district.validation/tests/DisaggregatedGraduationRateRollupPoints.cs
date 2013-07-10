38d887b4e9f5a192b37c6ee6fe417198cc925025
Jake Pearson <pearsonjj@yahoo.com>
2013-07-09 18:57:56 -0600

    Nulls allowed (Issue #15)

--- cde.district.validation/tests/DisaggregatedGraduationRateRollupPoints.cs
+++ cde.district.validation/tests/DisaggregatedGraduationRateRollupPoints.cs
@@ -12,6 +12,17 @@ namespace cde.district.validation.tests
 			AssertSum(row, "1YR_PS_GRAD_DSAG_PTS_EARN", new[] { "1YR_PS_GRAD_ELL_PTS_EARN", "1YR_PS_GRAD_IEP_PTS_EARN", "1YR_PS_GRAD_MIN_PTS_EARN", "1YR_PS_GRAD_FRL_PTS_EARN" }, errors);
 			AssertSum(row, "1YR_PS_GRAD_DSAG_PTS_ELIG", new[] { "1YR_PS_GRAD_ELL_PTS_ELIG", "1YR_PS_GRAD_IEP_PTS_ELIG", "1YR_PS_GRAD_MIN_PTS_ELIG", "1YR_PS_GRAD_FRL_PTS_ELIG" }, errors);
 			AssertSum(row, "3YR_PS_GRAD_DSAG_PTS_EARN", new[] { "3YR_PS_GRAD_ELL_PTS_EARN", "3YR_PS_GRAD_IEP_PTS_EARN", "3YR_PS_GRAD_MIN_PTS_EARN", "3YR_PS_GRAD_FRL_PTS_EARN" }, errors);
-			AssertSum(row, "3YR_PS_GRAD_DSAG_PTS_ELIG", new[] { "3YR_PS_GRAD_ELL_PTS_ELIG", "3YR_PS_GRAD_IEP_PTS_ELIG", "3YR_PS_GRAD_MIN_PTS_ELIG", "3YR_PS_GRAD_FRL_PTS_ELIG" }, errors);	}
+			AssertSum(row, "3YR_PS_GRAD_DSAG_PTS_ELIG", new[] { "3YR_PS_GRAD_ELL_PTS_ELIG", "3YR_PS_GRAD_IEP_PTS_ELIG", "3YR_PS_GRAD_MIN_PTS_ELIG", "3YR_PS_GRAD_FRL_PTS_ELIG" }, errors);
+        }
+
+        protected override bool AssertSum(Row row, string resultColumn, IEnumerable<string> partColumns, Errors errors)
+        {
+            if (!Defined(row, resultColumn, errors))
+            {
+                return true;
+            }
+
+            return base.AssertSum(row, resultColumn, partColumns, errors);
+        }
 	}
 }
