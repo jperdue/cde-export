1c96f8fe799ab9979d6bc2c706fea37b353d9dbe
Jake Pearson <pearsonjj@yahoo.com>
2013-07-09 18:54:48 -0600

    Nulls allowed (Issue #13)

--- cde.district.validation/tests/PwrPointsEarnedEqualsPercentOfPwrPoints.cs
+++ cde.district.validation/tests/PwrPointsEarnedEqualsPercentOfPwrPoints.cs
@@ -12,5 +12,15 @@ namespace cde.district.validation.tests
 			AssertDivide(row, "1YR_PS_PCT_PTS_EARN_TTL", "1YR_PS_PTS_EARN_TTL", "1YR_PS_PTS_ELIG_TTL", errors);
 			AssertDivide(row, "3YR_PS_PCT_PTS_EARN_TTL", "3YR_PS_PTS_EARN_TTL", "3YR_PS_PTS_ELIG_TTL", errors);
 		}
+
+        protected override bool AssertDivide(Row row, string resultColumn, string numeratorColumn, string denominatorColumn, Errors errors)
+        {
+            if (!Defined(row, resultColumn, errors))
+            {
+                return true;
+            }
+
+            return base.AssertDivide(row, resultColumn, numeratorColumn, denominatorColumn, errors);
+        }
 	}
 }
