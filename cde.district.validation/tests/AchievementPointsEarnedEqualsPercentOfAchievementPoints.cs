a80685e49189c519c92a232e674be318b6bd1ebf
Jake Pearson <pearsonjj@yahoo.com>
2013-07-14 20:54:16 -0600

    Null checks (Issue #18)

--- cde.district.validation/tests/AchievementPointsEarnedEqualsPercentOfAchievementPoints.cs
+++ cde.district.validation/tests/AchievementPointsEarnedEqualsPercentOfAchievementPoints.cs
@@ -12,5 +12,15 @@ namespace cde.district.validation.tests
 			AssertDivide(row, "1YR_ACH_PCT_PTS_EARN_TTL", "1YR_ACH_PTS_EARN_TTL", "1YR_ACH_PTS_ELIG_TTL", errors);
 			AssertDivide(row, "3YR_ACH_PCT_PTS_EARN_TTL", "3YR_ACH_PTS_EARN_TTL", "3YR_ACH_PTS_ELIG_TTL", errors);
 		}
+
+        protected override bool AssertDivide(Row row, string resultColumn, string numeratorColumn, string denominatorColumn, Errors errors)
+        {
+            if (!Defined(row, numeratorColumn, errors))
+            {
+                return true;
+            }
+
+            return base.AssertDivide(row, resultColumn, numeratorColumn, denominatorColumn, errors);
+        }
 	}
 }
