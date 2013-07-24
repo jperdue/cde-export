025f1dd066535f7c6875c453ee74507bbe4f2cc3
Jake Pearson <pearsonjj@yahoo.com>
2013-07-23 19:49:15 -0600

    Removed RDPF from indicator test

--- cde.district.validation/tests/IndicatorCount.cs
+++ cde.district.validation/tests/IndicatorCount.cs
@@ -9,10 +9,10 @@ namespace cde.district.validation.tests
     {
         public override void Test(Row row, Errors errors)
         {
-            var oneYear = new[] { "1YR_ACHIEVE_RATING", "1YR_GROWTH_RATING", "1YR_GRO_GAPS_RATING", "RDPF_1YR_POST_SEC_RATING" };
+            var oneYear = new[] { "1YR_ACHIEVE_RATING", "1YR_GROWTH_RATING", "1YR_GRO_GAPS_RATING", "1YR_POST_SEC_RATING" };
             AssertRating(row, "1YR_INDICATOR_NCOUNT", oneYear, errors, GetIndicatorCount);
 
-            var threeYear = new[] { "3YR_ACHIEVE_RATING", "3YR_GROWTH_RATING", "3YR_GRO_GAPS_RATING", "RDPF_3YR_POST_SEC_RATING" };
+            var threeYear = new[] { "3YR_ACHIEVE_RATING", "3YR_GROWTH_RATING", "3YR_GRO_GAPS_RATING", "3YR_POST_SEC_RATING" };
             AssertRating(row, "3YR_INDICATOR_NCOUNT", threeYear, errors, GetIndicatorCount);
         }
 
