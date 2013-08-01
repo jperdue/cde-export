7674e66a9854535384b4b46f04f8ffb12a96e328
Jake Pearson <pearsonjj@yahoo.com>
2013-07-31 21:33:32 -0600

    Growth Gaps Points Rollup null checks (fixes #25)

--- cde.district.validation/tests/GrowthGapsPointsRollup.cs
+++ cde.district.validation/tests/GrowthGapsPointsRollup.cs
@@ -29,5 +29,16 @@ namespace cde.district.validation.tests
 			AssertSum(row, "3YR_GG_PTS_EARN_TTL", new[] { "3YR_GG_PTS_EARN_READ", "3YR_GG_PTS_EARN_MATH", "3YR_GG_PTS_EARN_WRITE" }, errors);
 			AssertSum(row, "3YR_GG_PTS_ELIG_TTL", new[] { "3YR_GG_PTS_ELIG_READ", "3YR_GG_PTS_ELIG_MATH", "3YR_GG_PTS_ELIG_WRITE" }, errors);
 		}
+
+        public override bool AssertSum(Row row, string resultColumn, IEnumerable<string> partColumns, Errors errors)
+        {
+            if (!Defined(row, resultColumn, errors) &&
+                partColumns.All(c => !Defined(row, c, errors)))
+            {
+                return true;
+            }
+
+            return base.AssertSum(row, resultColumn, partColumns, errors);
+        }
 	}
 }
